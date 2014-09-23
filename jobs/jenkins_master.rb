375ee668a97dee93679f20ff8235dde1cae43b82
EZPK <thom.bruyer@gmail.com>
2014-09-23 16:55:10 +0200

    new dashboard

---
+++ jobs/jenkins_master.rb
@@ -0,0 +1,33 @@
+require 'net/http'
+require 'json'
+require 'time'
+
+JENKINS_URI = URI.parse("server")
+
+JENKINS_AUTH = {
+  'name' => 'user',
+  'password' => 'pwd'
+}
+
+def get_json_for_master_jenkins()
+    http = Net::HTTP.new(JENKINS_URI.host, JENKINS_URI.port)
+    request = Net::HTTP::Get.new("/jenkins/api/json?pretty=true")
+
+    if JENKINS_AUTH['name']
+        request.basic_auth(JENKINS_AUTH['name'], JENKINS_AUTH['password'])
+    end
+    response = http.request(request)
+    JSON.parse(response.body)
+end
+# the key of this mapping must be a unique identifier for your job, the according value must be the name that is specified in jenkins
+
+SCHEDULER.every '100s', :first_in => 0 do |job|
+    puts "\e[33mGILLIAN\e[0m"
+    thom = get_json_for_master_jenkins()
+
+send_event('master_jobs', 
+    jobs: thom['jobs'][-4..-1]
+)
+
+end
+
---
+++ widgets/jenkins_master/jenkins_master.coffee
@@ -0,0 +1,1 @@
+class Dashing.JenkinsMaster extends Dashing.Widget
