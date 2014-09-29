f474a8e82e4f59fbdbf7ecb78363189c13fa38ee
EZPK <thom.bruyer@gmail.com>
2014-09-29 17:12:57 +0200

    adding google API widget

--- jobs/visitor_count_real_time.rb
+++ jobs/visitor_count_real_time.rb
@@ -2,14 +2,14 @@ require 'google/api_client'
 require 'date'
 
 # Update these to match your own apps credentials
-service_account_email = '148511232865-0okif4mgdrgabroc08q30umv98umqepj@developer.gserviceaccount.com' # Email of service account
-key_file = '/home/bruyer/Bureau/756e2f003cc9.p12' # File containing your private key
+service_account_email = '855261708634-kk3nne6f3jgig7rdeoc5c6ptb6nomqgl@developer.gserviceaccount.com' # Email of service account
+key_file = '/home/bruyer/Bureau/cerisewidget.p12' # File containing your private key
 key_secret = 'notasecret' # Password to unlock private key
-profileID = 'UA-55151386-1' # AnalYtics profile ID.-55151386-1
+profile_id = '38650677' # AnalYtics profile ID.-55151386-1
 
 # Get the Google API client
 client = Google::APIClient.new(
-  :application_name => '[YOUR APPLICATION NAME]', 
+  :application_name => 'CERISE',
   :application_version => '0.01'
 )
 
@@ -24,23 +24,23 @@ client.authorization = Signet::OAuth2::Client.new(
   :issuer => service_account_email,
   :signing_key => key)
 
-# Start the scheduler
-SCHEDULER.every '10m', :first_in => 0 do
 
+# Start the scheduler
+SCHEDULER.every '10s', :first_in => 0 do
   # Request a token for our service account
   client.authorization.fetch_access_token!
 
   # Get the analytics API
   analytics = client.discovered_api('analytics','v3')
 
+
   # Execute the query
-  visitCount = client.execute(:api_method => analytics.data.realtime.get, :parameters => { 
+  response = client.execute(:api_method => analytics.data.realtime.get, :parameters => {
     'ids' => "ga:" + profile_id,
     'metrics' => "ga:activeVisitors",
   })
 
-  visitors << { x: Time.now.to_i, y: visits.to_i }
-
+  visitors << { x: Time.now.to_i, y: response.data.rows }
   # Update the dashboard
   send_event('visitor_count_real_time', points: visitors)
 end
