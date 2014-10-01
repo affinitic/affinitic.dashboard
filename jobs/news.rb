01079ded523408fc9363dd6d0352b8d1ce43a97b
EZPK <thom.bruyer@gmail.com>
2014-10-01 15:35:32 +0200

    Trying to solve data issues

--- jobs/news.rb
+++ jobs/news.rb
@@ -2,6 +2,7 @@ require 'net/http'
 require 'uri'
 require 'nokogiri'
 require 'htmlentities'
+require_relative 'rss_library'
 
 myfile = File.open('../urlbitbucket.json', 'r')
 myurl = JSON.parse(myfile.read)
@@ -10,52 +11,17 @@ news_feeds = {
   "bitbucket" => myurl['url']
 }
 
-Decoder = HTMLEntities.new
-
-class News
-  def initialize(widget_id, feed)
-    @widget_id = widget_id
-    uri = URI.parse(feed)
-    @path = uri.path + '?' + uri.query
-    @http = Net::HTTP.new(uri.host, uri.port)
-    @http.use_ssl = true
-  end
-
-  def widget_id()
-    @widget_id
-  end
-
-  def latest_headlines()
-    response = @http.request(Net::HTTP::Get.new(@path))
-    doc = Nokogiri::XML(response.body)
-    news_headlines = [];
-    doc.xpath('//channel/item').each do |news_item|
-      title = clean_html( news_item.xpath('title').text )
-      summary = clean_html( news_item.xpath('description').text )
-      news_headlines.push({ title: title, description: summary })
-    end
-    news_headlines
-  end
-
-  def clean_html( html )
-    html = html.gsub(/<\/?[^>]*>/, "")
-    html = Decoder.decode( html )
-    return html
-  end
-
-end
-
-@News = []
+news_list = []
 news_feeds.each do |widget_id, feed|
   begin
-    @News.push(News.new(widget_id, feed))
+    news_list.push(RssNews.new(widget_id, feed, use_ssl=true))
   rescue Exception => e
     puts e.to_s
   end
 end
 
-SCHEDULER.every '60m', :first_in => 0 do |job|
-  @News.each do |news|
+SCHEDULER.every '5s', :first_in => 0 do |job|
+  news_list.each do |news|
     headlines = news.latest_headlines()
     send_event(news.widget_id, { :headlines => headlines })
   end
