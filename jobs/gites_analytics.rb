10e7b67dcec61d92f6ffb727b03791c5a51429ba
EZPK <thom.bruyer@gmail.com>
2014-10-01 15:44:19 +0200

    changing job name

---
+++ jobs/gites_analytics.rb
@@ -0,0 +1,53 @@
+require 'google/api_client'
+require 'date'
+
+myfile = File.open('../pwdgoogleapirss.json', 'r')
+myinfo = JSON.parse(myfile.read)
+
+
+service_account_email = myinfo['service_account_email'] #Email of service account
+key_file = myinfo['key_file'] # File containing your private key
+key_secret = myinfo['key_secret'] # Password to unlock private key
+profile_id = myinfo['profile_id'] # AnalYtics profile ID.-55151386-1
+
+
+# Get the Google API client
+client = Google::APIClient.new(
+  :application_name => 'Gites Wallonie',
+  :application_version => '0.01'
+)
+
+visitors = []
+
+# Load your credentials for the service account
+key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
+client.authorization = Signet::OAuth2::Client.new(
+  :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+  :audience => 'https://accounts.google.com/o/oauth2/token',
+  :scope => 'https://www.googleapis.com/auth/analytics.readonly',
+  :issuer => service_account_email,
+  :signing_key => key)
+
+
+# Start the scheduler
+SCHEDULER.every '10s', :first_in => 0 do
+  # Request a token for our service account
+  client.authorization.fetch_access_token!
+
+  # Get the analytics API
+  analytics = client.discovered_api('analytics','v3')
+
+
+  # Execute the query
+  response = client.execute(:api_method => analytics.data.realtime.get, :parameters => {
+    'ids' => "ga:" + profile_id,
+    'metrics' => "ga:activeVisitors",
+  })
+
+  visitors << { x: Time.now.to_i, y: response.data.rows[0][0].to_i }
+  if visitors.length > 10 then
+      visitors.delete(0)
+  end
+  # Update the dashboard
+  send_event('rss', points: visitors)
+end
