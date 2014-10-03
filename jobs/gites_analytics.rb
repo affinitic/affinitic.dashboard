c0dbd367b1f7cf8b9f1009a9526198172729bfee
EZPK <thom.bruyer@gmail.com>
2014-10-03 16:44:42 +0200

    changing sec for min

--- jobs/gites_analytics.rb
+++ jobs/gites_analytics.rb
@@ -2,6 +2,6 @@ require_relative 'google_analytics_library'
 
 gites_analytics = GoogleAnalytics.new('../pwdgoogleapirss.json', 'GITES')
 
-SCHEDULER.every '10s', :first_in => 0 do
+SCHEDULER.every '10m', :first_in => 0 do
     send_event('gites_analytics', points: gites_analytics.GetVisitors())
 end
