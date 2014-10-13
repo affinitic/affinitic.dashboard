f3ddcf0dcadf37a2fc1335633fc0bb8e41c7eb7c
EZPK <thom.bruyer@gmail.com>
2014-10-13 16:34:14 +0200

    deleting useless widgets

--- jobs/cerise_analytics.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'google_analytics_library'
-
-cerise_analytics = GoogleAnalytics.new('pass/pwdgoogleapi.json', 'ARSIA')
-
-SCHEDULER.every '10m', :first_in => 0 do
-    send_event('cerise_analytics', points: cerise_analytics.GetVisitors())
-end
