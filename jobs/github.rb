0344d7eca83f8aae78f0f053f15e50299632bc32
EZPK <thom.bruyer@gmail.com>
2014-10-14 16:45:53 +0200

    adding github issues job

---
+++ jobs/github.rb
@@ -0,0 +1,21 @@
+require 'github_api'
+
+myfile = File.open('pass/pwdgithub.json', 'r')
+myinfo = JSON.parse(myfile.read)
+
+connection = Github.new :oauth_token => myinfo['token'] 
+pulls=0
+
+issues = connection.issues.list(:org => 'affinitic', :filter => 'all', :auto_pagination => true)
+issues.each do |issue|
+  if issue["pull_request"]
+    pulls += 1
+  end
+end
+
+
+SCHEDULER.every '100s', :first_in => 0 do
+  send_event('github_feed', pulls:pulls)
+end
+
+
