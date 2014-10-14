bf3dc373e0d7718782863d6ed94d678265187cb6
EZPK <thom.bruyer@gmail.com>
2014-10-14 09:53:48 +0200

    new version of trac_push

--- trac_push.py
+++ trac_push.py
@@ -2,36 +2,26 @@ import json
 import requests
 
 dashboard_url = "http://localhost:3030"
-widget_url = dashboard_url + '/widgets/trac'
+widget_url = dashboard_url + '/widgets/trac-arsia'
 
-datas = [
-    {
+datas = {
         "value":"1",
         "class": "blocker_tickets",
         "text": "Blocker tickets",
-    },
-    {
-        "value":"5",
-        "class":"opened_today",
-        "text":"Opened today",
-    },
-    {
-        "value":"3",
-        "class":"closed_today",
-        "text":"Closed today",
-    },
-]
+    }
+
 
 nested_dict = {'auth_token':"blablabla12345",
-               'items':datas}
+               'item':datas}
+
 
 json_object = json.dumps(nested_dict)
 headers = {'Content-Type': 'application/json', 'Accept': 'text/plain'}
 
 
 try:
-     requests.post(widget_url, json_object, headers=headers)
-     print json_object
+    requests.post(widget_url, json_object, headers=headers)
+    print json_object
 except IOError as e:
     if hasattr(e, 'reason'):
         print "connection error:", e.reason
@@ -40,22 +30,4 @@ except IOError as e:
         print e.read()
     else:
         print "error:", e
-
-
-
-blocker_count =0
-open_count = 0
-closed_count = 0
-
-for i in range(len(datas)):
-    if datas[i]['value'] == 'critical_tickets()':
-        blocker_count+=1
-    if datas[i]['value'] == 'closed_tickets()':
-        closed_count+=1
-    if datas[i]['value'] == 'opened_tickets()':
-        open_count+=1
-
-print 'blocker_count = ' + str(blocker_count)
-print 'open_count = ' + str(open_count)
-print 'closed_count = ' + str(closed_count)
 
