b05853e9e8a2ad1ce228ca880938ab6c00931cce
EZPK <thom.bruyer@gmail.com>
2014-10-14 17:07:03 +0200

    fixing issues display problem

--- jobs/github.rb
+++ jobs/github.rb
@@ -5,17 +5,19 @@ myinfo = JSON.parse(myfile.read)
 
 connection = Github.new :oauth_token => myinfo['token'] 
 pulls=0
-
+issuesCount=0
 issues = connection.issues.list(:org => 'affinitic', :filter => 'all', :auto_pagination => true)
 issues.each do |issue|
   if issue["pull_request"]
     pulls += 1
+  else
+    issuesCount+=1
   end
 end
 
 
 SCHEDULER.every '100s', :first_in => 0 do
-  send_event('github_feed', pulls:pulls)
+  send_event('github_feed', pulls:pulls, issues:issuesCount)
 end
 
 
