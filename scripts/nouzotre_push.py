c151a4e812cb0c18fd8a5cd3c2e21a0e0095a921
EZPK <thom.bruyer@gmail.com>
2014-10-21 10:16:56 +0200

    changing the way the auth_token is working

--- scripts/nouzotre_push.py
+++ scripts/nouzotre_push.py
@@ -4,36 +4,42 @@ import requests
 dashboard_url = "http://localhost:3030"
 widget_url = dashboard_url + '/widgets/nouzotre'
 
-datas ={
-    "milestone":{
-        "prestation":"bonjour",
-        "date":"10 03 2018",
-        "deadlines":"Blocker tickets",
-        "photo":"assets/logo.png"
+datas = {
+    "milestone": {
+        "prestation": "bonjour",
+        "date": "10 03 2018",
+        "deadlines": "Blocker tickets",
+        "photo": "assets/logo.png"
         }
 }
 
-mynouzotre = open('pass/nouzotre.json')
-test = json.load(mynouzotre)
-mynouzotre.close()
+
+def get_token():
+    mynouzotre = open('pass/configru.json')
+    myinfo = json.load(mynouzotre)
+    mynouzotre.close()
+    return myinfo['pwd']
 
 
-nested_dict = {"auth_token":test["pwd"],
-               "item":datas}
+def main():
+    token = get_token()
+    nested_dict = {"auth_token": token,
+                "item": datas}
 
+    json_object = json.dumps(nested_dict)
+    headers = {"Content-Typer": "application/json"}
 
-json_object = json.dumps(nested_dict)
-headers = {"Content-Typer": "application/json" }
+    try:
+        requests.post(widget_url, json_object, headers=headers)
+        print json_object
+    except IOError as e:
+        if hasattr(e, 'reason'):
+            print "connection error:", e.reason
+        elif hasattr(e, 'code'):
+            print "http error:", e.code
+            print e.read()
+        else:
+            print "error:", e
 
-
-try:
-    requests.post(widget_url, json_object, headers=headers)
-    print json_object
-except IOError as e:
-    if hasattr(e, 'reason'):
-        print "connection error:", e.reason
-    elif hasattr(e, 'code'):
-        print "http error:", e.code
-        print e.read()
-    else:
-        print "error:", e
+if __name__ == "__main__":
+    main()
