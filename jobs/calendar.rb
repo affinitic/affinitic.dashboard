44a79f61f8ce774f77d20ba3f041eabf0b750440
schminitz <gillian@affinitic.be>
2015-01-09 15:43:18 +0100

    Add calendar widget

---
+++ jobs/calendar.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+require 'open-uri'
+require 'nokogiri'
+require 'date'
+require 'cgi'
+
+# Config
+# make sure your URLs end with /full, not /simple (which is default)!
+# ------
+calendars = [{name: 'Private', url: 'https://www.google.com/calendar/feeds/gillian.sampont%40gmail.com/private-fd8af6600f8654a9025605f6a9acc0d8/basic'}]
+events = Array.new
+
+SCHEDULER.every '30s', :first_in => 0 do |job|
+	events = Array.new
+	min = CGI.escape(DateTime.now().to_s)
+	max = CGI.escape((DateTime.now()+7).to_s)
+	
+	calendars.each do |calendar|
+		url = calendar[:url]+"?singleevents=true&orderby=starttime&start-min=#{min}&start-max=#{max}&hl=fr"
+		reader = Nokogiri::XML(open(url))
+		reader.remove_namespaces!
+		reader.xpath("//feed/entry").each do |e|
+			title = e.at_xpath("./title").text
+			content = e.at_xpath("./content").text
+			when_node = e.at_xpath("./when")
+			events.push({title: title,
+				body: content ? content.split("\n").first.split("Date : ").last : "",
+				calendar: calendar[:name],
+				when_start_raw: when_node ? DateTime.iso8601(when_node.attribute('startTime').text).to_time.to_i : 0,
+				when_end_raw: when_node ? DateTime.iso8601(when_node.attribute('endTime').text).to_time.to_i : 0,
+				when_start: when_node ? DateTime.iso8601(when_node.attribute('startTime').text).to_s : "No time",
+				when_end: when_node ? DateTime.iso8601(when_node.attribute('endTime').text).to_s : "No time"
+			})
+		end
+	end
+
+  events.sort! { |a,b| a[:when_start_raw] <=> b[:when_start_raw] }
+	events = events.slice!(0,15) # 15 elements is probably enough...
+  events = events.reverse
+
+  status = ''
+  if events.length == 0
+    status = 'Aucun evenement futur'
+  end
+	
+	send_event('calendar_events',
+             events: events,
+             status: status)
+end
