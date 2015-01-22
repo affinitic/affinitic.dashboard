196d67c46a59af1fdd8b65d574e196bd89442984
schminitz <gillian@affinitic.be>
2015-01-22 15:51:10 +0100

    Get previous events + change some style affinitic refs #6122

--- jobs/calendar.rb
+++ jobs/calendar.rb
@@ -16,8 +16,8 @@ events = Array.new
 
 SCHEDULER.every '100s', :first_in => 0 do |job|
 	events = Array.new
-	min = CGI.escape(DateTime.now().to_s)
-	max = CGI.escape((DateTime.now()+7).to_s)
+	min = CGI.escape((DateTime.now()-7).to_s)
+	max = CGI.escape((DateTime.now()+365).to_s)
 	
 	calendars.each do |calendar|
 		url = calendar[:url]+"?singleevents=true&orderby=starttime&start-min=#{min}&start-max=#{max}&hl=fr"
